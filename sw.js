bb5a0df2d8df11c507880f016dc2c3b12c751842
wttm-labs <274231192+wttm-labs@users.noreply.github.com>
2026-08-14 00:06:51 +0200

    Erfolge ins Profil verschoben, Medaillen als minimalistische SVG-Icons statt Emoji

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "angelique-v6";
+const CACHE = "angelique-v7";
 const DATEIEN = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
