7c41eb8c5aa6b08d0e0a5515858579cf38192fa6
wttm-labs <274231192+wttm-labs@users.noreply.github.com>
2026-08-16 22:19:44 +0200

    Alles-löschen, Dopplungs-Warnung beim Import, Erkennung wiederkehrender Zahlungen, lernende Kategorisierung

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "angelique-v7";
+const CACHE = "angelique-v8";
 const DATEIEN = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
