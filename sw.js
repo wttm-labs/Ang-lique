7c369637951093faa81157ec99a65a34247e15ac
wttm-labs <274231192+wttm-labs@users.noreply.github.com>
2026-08-17 14:52:45 +0200

    CSV-Datei direkt aus dem Dateisystem auswählen statt nur Text einfügen

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "angelique-v8";
+const CACHE = "angelique-v9";
 const DATEIEN = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
