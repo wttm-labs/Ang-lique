db9fc73132f8cd2987b02ebb6292c81bf0e1db3b
wttm-labs <274231192+wttm-labs@users.noreply.github.com>
2026-08-17 16:01:53 +0200

    Alles-löschen: eigene Bestätigungsseite statt natives confirm() (funktioniert auch als installierte iOS-PWA)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "angelique-v11";
+const CACHE = "angelique-v12";
 const DATEIEN = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
