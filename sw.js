897f914dd573c5efae2f4dc0fa812d8c72d8971d
wttm-labs <274231192+wttm-labs@users.noreply.github.com>
2026-08-17 16:45:51 +0200

    Referenzwerte aus echten Daten berechnen (überschreibbar), keine Platzhalter mehr bei Neuinstallation, Einnahmen-Tracking mit Saldo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "angelique-v12";
+const CACHE = "angelique-v13";
 const DATEIEN = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
